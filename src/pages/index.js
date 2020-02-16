ff013a480f3f73827832bdd60eb9453a364372e0
Artsiom_Reutovich <artsiom_reutovich@epam.com>
2020-02-16 23:57:22 +0300

    init: added base storybook configuration / added sample of component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,14 @@
 import React from 'react';
+import Button from '../components/Button';
 
-export default () => <div>Hello world!</div>;
+export default () => (
+  <div>
+    Hello world!
+
+    <Button
+      type="button"
+      content="click me"
+      onClickCallback={(e) => { console.warn(e.target); }}
+    />
+  </div>
+);
