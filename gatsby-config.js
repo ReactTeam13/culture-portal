c8ffc5effe2ae2b7faf2465097f5a34d1a5136a0
Artsiom_Reutovich <artsiom_reutovich@epam.com>
2020-02-16 21:45:23 +0300

    init: added base eslint configuration/ refactoring regarding new eslint configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,17 @@
  */
 
 module.exports = {
-  /* Your site config here */
-}
+  plugins: [
+    {
+      resolve: 'gatsby-plugin-eslint-v2',
+      options: {
+        test: /\.js$|\.jsx$/,
+        exclude: /(node_modules|cache|public)/,
+        options: {
+          emitWarning: true,
+          failOnError: false,
+        },
+      },
+    },
+  ],
+};
