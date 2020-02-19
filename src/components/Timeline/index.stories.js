32735160d91b647351ddf7e97a2de4e90eb7f9e8
Artsiom_Reutovich <artsiom_reutovich@epam.com>
2020-02-19 23:18:48 +0300

    fix: fix timeline stories

--- src/components/Timeline/index.stories.js
+++ src/components/Timeline/index.stories.js
@@ -1,28 +1,34 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { TimelineItem } from 'vertical-timeline-component-for-react';
-import Timeline from './index';
+import TimeLine from './index';
 
 const index = 1;
-const item = {
-  date: '1990',
-  place: 'Минск',
-  description: 'Описание деятельности'
-};
+const data = {activity: [
+  {
+    date: '13.01.1914',
+    place: 'с.Ивановка',
+    description: 'Родился в с.Ивановка Донецкой области Украины.',
+  },
+  {
+    date: '1932-1940',
+    place: 'Харьков',
+    description: 'Учился в Харьковском архитектурно-строительном техникуме, а затем в Харьковском инженерно-строительном институте',
+  },
+  {
+    date: '1940-1944',
+    place: 'Польша',
+    description: 'Был призван в армию, сражался с фашистами, два года провёл в плену, бежал из плена, примкнул к партизанам.',
+  },
+  {
+    date: '1944-1980',
+    place: 'Минск',
+    description: 'На протяжении 36 лет работал в Белгоспроекте. Прошёл путь от рядового архитектора до руководителя архитектурно-конструкторской мастерской.',
+  },
+]}
 
 storiesOf('Timeline', module)
   .add('Timeline', () => (
-    <div>
-      <h2>Биография архитектора</h2>
-
-          <TimelineItem
-            key={index}
-            dateText={item.date}
-            dateInnerStyle={{ background: '#e86971', color: '#fff' }}
-          >
-            <h3>{item.place}</h3>
-            <p>{item.description}</p>
-          </TimelineItem>
-
-    </div>
+      <TimeLine
+        data={data}
+      />
   ));
