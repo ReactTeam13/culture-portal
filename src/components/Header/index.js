d465e1c818c7e5008dc343f5a5adb1c59c575738
Artsiom_Reutovich <artsiom_reutovich@epam.com>
2020-02-20 20:08:20 +0300

    feat: add story for header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -9,20 +9,24 @@ function Header() {
   return (
     <header className="container py-3">
       <div className="row justify-content-between align-items-center">
-        <div className="col-12 order-1 text-center mb-5 col-md order-md-1 mb-md-0 text-center">
+        <div className="col-12 order-1 text-center mb-5 col-md order-md-1 mb-md-0 text-md-left logo-wrapper">
           <Logo />
         </div>
 
-        <nav className="col-12 order-3 text-center mb-4 col-md order-md-2 mb-md-0 text-center">
-          <Link to="/" className="p-4 d-block p-md-2 d-md-inline-block navigation-link">
-            Главная
-          </Link>
-          <Link to="/architectors" className="p-4 d-block p-md-2 d-md-inline-block navigation-link">
-            Архитекторы
-          </Link>
+        <nav className="col-12 order-3 text-center mb-4 col-md order-md-2 mb-md-0 navigation">
+          <div className="d-block p-4 d-md-inline-block w-50 text-center">
+            <Link to="/" className="navigation-link">
+              Главная
+            </Link>
+          </div>
+          <div className="d-block p-4 d-md-inline-block w-50 text-center">
+            <Link to="/architectors" className="navigation-link">
+              Архитекторы
+            </Link>
+          </div>
         </nav>
 
-        <div className="col-12 order-2 text-center mb-4 col-md order-md-3 mb-md-0">
+        <div className="col-12 order-2 text-center mb-4 col-md order-md-3 mb-md-0 text-md-right language-switcher-wrapper">
           {/*  Place buttons here, once is will be ready */}
           <div>
             <button>111</button>
