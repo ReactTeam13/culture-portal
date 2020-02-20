ec54ff19acb8039c65a7b4a92b2920e1a513d5a0
Artsiom_Reutovich <artsiom_reutovich@epam.com>
2020-02-20 18:50:46 +0300

    feat: add story for header

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -6,7 +6,7 @@ const Logo = () => (
   <h1 className="main-logo">
     <a href="/" title="Culture portal" className="d-inline-block main-logo--link">
       <img className="d-inline-block" src={LogoImage} alt="Culture portal" />
-      <span className="d-inline-block pt-2 pt-md-0 main-logo--description">
+      <span className="d-inline-block px-2 pt-2 pt-md-0 main-logo--description">
         Архитекторы Беларуси
       </span>
     </a>
