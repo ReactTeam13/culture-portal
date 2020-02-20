aff5a47a317f23819841e40639bd6cd9e855bb42
Anthon_M <xll2005@tut.by>
2020-02-20 19:31:33 +0300

    refactor: change translation files

---
+++ src/translations/index.js
@@ -0,0 +1,12 @@
+import ru from './ru';
+import en from './en';
+import be from './be';
+import langs from './languages';
+
+export const locales = {
+  be,
+  en,
+  ru,
+};
+
+export const languages = langs;
