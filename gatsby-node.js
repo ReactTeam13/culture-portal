d1c2ee2bb06f07b70df4496f2eee42b8c9c2a8e1
Anthon_M <xll2005@tut.by>
2020-02-22 13:12:30 +0300

    feat: add page generation using contentful

--- gatsby-node.js
+++ gatsby-node.js
@@ -1462,23 +1462,124 @@ const architectors = [
   },
 ];
 
-exports.createPages = ({ actions }) => {
+console.log(architectors);
+
+const architectQuery = `
+query MyQuery {
+  allContentfulArchitect {
+    nodes {
+      data {
+        be {
+          activity {
+            description
+            place
+            date
+          }
+          birthDate
+          avatar
+          birthPlace
+          deathDate
+          deathPlace
+          id
+          listOfWorks {
+            work
+            year
+          }
+          locationsCoords {
+            lat
+            lng
+          }
+          name
+          occupation
+          videoId
+          works {
+            description
+            image
+          }
+        }
+        en {
+          activity {
+            date
+            description
+            place
+          }
+          avatar
+          birthDate
+          deathDate
+          birthPlace
+          deathPlace
+          id
+          listOfWorks {
+            work
+            year
+          }
+          locationsCoords {
+            lat
+            lng
+          }
+          videoId
+          occupation
+          name
+          works {
+            description
+            image
+          }
+        }
+        ru {
+          activity {
+            date
+            description
+            place
+          }
+          birthDate
+          avatar
+          birthPlace
+          deathDate
+          deathPlace
+          id
+          listOfWorks {
+            work
+            year
+          }
+          locationsCoords {
+            lat
+            lng
+          }
+          name
+          occupation
+          videoId
+          works {
+            description
+            image
+          }
+        }
+      }
+    }
+  }
+}
+`;
+
+exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions;
   return new Promise((resolve) => {
     const ArchitectorTemplate = path.resolve('src/templates/architector.js');
     resolve(
-      architectors.forEach((architector) => {
-        const urlName = architector.en.name.split(' ')[0];
-        const url = `architector/${urlName}`;
-        createPage({
-          path: url,
-          component: ArchitectorTemplate,
-          context: {
-            architector,
-          },
-        });
-        resolve();
-      }),
+      graphql(architectQuery)
+        .then(({ data: { allContentfulArchitect: { nodes } } }) => {
+          nodes.forEach(({ data: architector }) => {
+            const lastNameIndex = 0;
+            const urlName = architector.en.name.split(' ')[lastNameIndex];
+            const url = `architector/${urlName}`;
+            createPage({
+              path: url,
+              component: ArchitectorTemplate,
+              context: {
+                architector,
+              },
+            });
+            resolve();
+          });
+        }),
     );
   });
 };
