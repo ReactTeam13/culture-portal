2282324d9eae2deb297ac4f14bfb0d5a1ae85bf5
Anthon_M <xll2005@tut.by>
2020-02-24 13:58:52 +0300

    refactor: refactor MainPage component

--- src/containers/MainPage/index.js
+++ src/containers/MainPage/index.js
@@ -5,35 +5,23 @@ import ArchitectBlock from '../../components/ArchitectBlock';
 import Button from '../../components/Button';
 import architects from '../../data/architects';
 import ParalaxBlock from '../../components/ParalaxBlock';
-import Avatar from '../../components/Avatar';
-import Description from '../../components/Description';
 
-function MainPage({ onButtonClick }) {
+function MainPage() {
   const authorDay = Math.floor(Math.random() * architects.length);
   const profile = architects[authorDay].ru;
-  const url = architects[authorDay].en.name.split(' ')[0];
+  const lastNameIndex = 0;
+  const urlName = architects[authorDay].en.name.split(' ')[lastNameIndex];
   return (
     <div className="main-page">
       <ParalaxBlock />
-      <h2 className="title-mainpage">Автор дня</h2>
-      <Avatar data={profile} />
-      <Description data={profile} />
       <h2 className="text-uppercase text-center mb-4 title-mainpage">Архитектор дня</h2>
       <ArchitectBlock data={profile} />
-      <Link
-        to={`/architector/${url}`}
-        id={authorDay}
-        onClick={onButtonClick}
-      >
+      <Link to={`/architector/${urlName}`}>
         <Button
           type="button"
           content="Читать далее"
         />
       </Link>
-      <p className="description-mainpage">
-        <span>Добро пожаловать на портал,</span>
-        <span>посвященный архитекторам Беларуси.</span>
-      </p>
     </div>
   );
 }
