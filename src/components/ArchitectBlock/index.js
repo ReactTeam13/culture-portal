342fdde2a8fa047941d8875d16908de2e5bd8481
maryna-js <rastargueva.m@gmail.com>
2020-02-25 12:52:18 +0300

    feat: add margin for ArchitectBlock

--- src/components/ArchitectBlock/index.js
+++ src/components/ArchitectBlock/index.js
@@ -5,7 +5,7 @@ import DescriptionShort from '../DescriptionShort';
 
 function ArchitectBlock({ data, btn }) {
   return (
-    <div className="container py-3">
+    <div className="container py-3 architect">
       <div className="row">
         <Avatar data={data} />
         <DescriptionShort data={data} btn={btn} />
