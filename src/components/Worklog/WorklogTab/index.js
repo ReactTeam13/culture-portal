be3cde9e37370b64b478debf080bab21fd16f1d1
Ksenia Kuntsevich <kse.kuntsevich@icloud.com>
2020-02-25 19:49:14 +0300

    feat: add data for each person

--- src/components/Worklog/WorklogTab/index.js
+++ src/components/Worklog/WorklogTab/index.js
@@ -4,26 +4,35 @@ import './index.css';
 
 function WorklogTab({ data }) {
   return (
-    <div className="container py-3">
-      <table className="table table-bordered table-striped table-responsive-stack">
-        <thead className="thead-dark">
-          <tr>
-            <th>Time Spent</th>
-            <th>Feature</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.map((row, i) => (
-            <tr key={i}>
-              {
+    <div>
+      {
+    data.map((person, c) => (
+
+      <div key={c} className="container py-3">
+        <p>{person.name}</p>
+        <table className="table table-bordered table-striped table-responsive-stack">
+          <thead className="thead-dark">
+            <tr>
+              <th>Time Spent</th>
+              <th>Feature</th>
+            </tr>
+          </thead>
+          <tbody>
+            {person.data.map((row, i) => (
+              <tr key={i}>
+                {
                 row.map((cell, j) => (
                   <td key={j}>{cell}</td>
                 ))
             }
-            </tr>
-          ))}
-        </tbody>
-      </table>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+
+    ))
+  }
     </div>
   );
 }
