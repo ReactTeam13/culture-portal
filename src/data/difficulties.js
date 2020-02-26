ae89c5fcdbb77830a9f22f4486715ed3bf54d3f3
maryna-js <rastargueva.m@gmail.com>
2020-02-26 20:51:58 +0300

    feat: add styles for Work Difficulties

--- src/data/difficulties.js
+++ src/data/difficulties.js
@@ -3,17 +3,17 @@ const difficultiesList = {
     data: [
       {
         title: 'Process Organization and Communication',
-        icon: '../../../../static/img/worklog-img/icon01.png',
+        icon: '../img/worklog-img/icon01.png',
         description: 'Take into account the opinion of everyone, seek a compromise. Division of work into tasks.',
       },
       {
         title: 'Technical Challenge and Integration',
-        icon: '../../../../static/img/worklog-img/icon02.png',
+        icon: '../img/worklog-img/icon02.png',
         description: 'Research, installation and settings of GatsbyJS, CMS.',
       },
       {
         title: 'Working in One Repository, Merge Conflicts',
-        icon: '../../../../static/img/worklog-img/icon03.png',
+        icon: '../img/worklog-img/icon03.png',
         description: 'Organization of the process and rules of commits and pull requests.',
       }],
   },
@@ -21,17 +21,17 @@ const difficultiesList = {
     data: [
       {
         title: 'Арганізацыя працэсу і камунікацыя',
-        icon: '../../../../static/img/worklog-img/icon01.png',
+        icon: '../img/worklog-img/icon01.png',
         description: 'Ўлічваць інтарэсы і меркаванне кожнага, шукаць кампраміс. Разбіўка ўсёй працы на мікратаскі.',
       },
       {
         title: 'Тэхнічная частка і інтэграцыі',
-        icon: '../../../../static/img/worklog-img/icon02.png',
+        icon: '../img/worklog-img/icon02.png',
         description: 'Вывучэнне, ўстаноўка і настройка GatsbyJS, CMS.',
       },
       {
         title: 'Праца ў адным рэпазітары.',
-        icon: '../../../../static/img/worklog-img/icon03.png',
+        icon: '../img/worklog-img/icon03.png',
         description: 'Канфлікты зліцця галінак. Арганізацыя працэсу і правілаў камітаў і пул рэквэстаў.',
       },
 
@@ -41,17 +41,17 @@ const difficultiesList = {
     data: [
       {
         title: 'Организация процесса и общение',
-        icon: '../../static/img/worklog-img/icon01.png',
+        icon: '../img/worklog-img/icon01.png',
         description: 'Учитывать интересы и мнение каждого, искать компромисс. Разделение всей работы на микротаски.',
       },
       {
         title: 'Техническая часть и интеграции',
-        icon: '../../../../static/img/worklog-img/icon02.png',
+        icon: '../img/worklog-img/icon02.png',
         description: 'Изучение, установка и настройка GatsbyJS, CMS.',
       },
       {
         title: 'Работа в одном репозитории',
-        icon: '../../../../static/img/worklog-img/icon03.png',
+        icon: '../img/worklog-img/icon03.png',
         description: 'Кофликты слияния веток. Организация процесса и правил коммитов и пулл реквестов. ',
       }],
   },
