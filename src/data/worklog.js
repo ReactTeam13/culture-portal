0c1e8206e502e8a777823540d9c241f8ca33bb8d
maryna-js <rastargueva.m@gmail.com>
2020-02-27 14:50:34 +0300

    feat: update data for worklog table

--- src/data/worklog.js
+++ src/data/worklog.js
@@ -8,6 +8,8 @@ const worklog = {
           ['1h', 'Translate data'],
           ['2.5h', 'Investigate parallax library, prepare paralax block pictures'],
           ['5.5h', 'Create autor page (figma) layout, implement pf main page parallax block'],
+          ['4h', 'Work with timeline block, table with author works, video, map block'],
+          ['5h', 'Styleguide: page logo, colors, fonts'],
         ],
       },
       {
@@ -17,6 +19,9 @@ const worklog = {
           ['4h', 'Add material UI, migrate implemented components to gatsby environment'],
           ['1.5h', 'Investigate story book, which addon we can use to represent typography and etc. Fix storybook'],
           ['1.5h', 'Storybook info-addon storybook decorator, documentation.'],
+          ['3h', 'Implement Logo, Header components.'],
+          ['2h', 'Implement Search Component.'],
+          ['2h', 'Search Component, refactoring.'],
         ],
       },
       {
@@ -29,6 +34,9 @@ const worklog = {
           ['2h', 'Button component'],
           ['2h', 'Translate data'],
           ['3h', 'Main page block, Architect block, Button style and position'],
+          ['3.5h', 'Worklog page, responsive table.'],
+          ['4h', 'Block with difficulties at Worklog Component, translation of block'],
+          ['2h', 'Worklog data update, translation of updates'],
 
         ],
       },
@@ -53,6 +61,19 @@ const worklog = {
           ['5h', 'Translate, add architects data. Footer layout, footer data, data translation'],
           ['7h', 'Team page: layout, fix responsive style, add parallax team photo block, add title, translations, change contribution for each member'],
           ['8h', 'Creating two parallax components, add on page'],
+          ['3.5h', 'Worklog data to object, add worklog transtation. Add worklog table.'],
+          ['3.5h', 'Self-evaluation with checkboxes and list of all requirements for Worklog Components'],
+          ['3h', 'Worklog layout'],
+        ],
+      },
+      {
+        name: 'Irina',
+        data: [
+          ['3h', 'Project structure.'],
+          ['2h', 'Add list of works'],
+          ['2h', 'Add modal video'],
+          ['2h', 'Add search panel'],
+          ['4h', 'Translation of interface and author data'],
         ],
       }],
   },
@@ -65,6 +86,8 @@ const worklog = {
           ['1h', 'Пераклад дадзеных'],
           ['2.5h', 'Даследаваць бібліятэку parallax, падрыхтаваць малюнка блока parallax'],
           ['5.5h', 'Стварыць макет старонкі аўтара (figma), рэалізаваць блок паралакс галоўнай старонкі'],
+          ['4h', 'Праца з блокам часовай шкалы, табліца з аўтарскімі работамі, відэа, блок карты'],
+          ['5h', 'Кіраўніцтва па стылі: лагатып старонкі, колеры, шрыфты'],
         ],
       },
       {
@@ -74,7 +97,9 @@ const worklog = {
           ['4h', 'Дадаць карыстацкага інтэрфейсу, перанесці укаранёныя кампаненты на сераду gatsby'],
           ['1.5h', 'Даследаванне storybook, выкарыстанне для прадстаўлення друкарні і т. д. Фікс storybook'],
           ['1.5h', 'Storybook info-addon, storybook, дакументацыя.'],
-
+          ['3h', 'Рэалізацыя кампанентаў Logo и Header.'],
+          ['2h', 'Рэалізацыя кампанента пошуку.'],
+          ['2h', 'Кампанент пошуку, рэфактарынг.'],
         ],
       },
       {
@@ -86,7 +111,9 @@ const worklog = {
           ['2h', 'Button кампанент'],
           ['2h', 'Пераклад дадзеных'],
           ['3h', 'Блок галоўнай старонкі, Блок архітэктара, Стыль і становішча кнопкі'],
-
+          ['3.5h', 'Старонка Worklog, адаптыўная табліца.'],
+          ['4h', 'Блок з цяжкасцямі ў кампаненце Worklog, пераклад блока'],
+          ['2h', 'Абнаўленьні дадзеных Worklog, пераклад абнаўленняў'],
         ],
       },
       {
@@ -106,8 +133,21 @@ const worklog = {
           ['3.5h', 'Збор дадзеных, падзел задач, даследаванне аб CMS.'],
           ['4.5h', 'Team, Link, TeamAvatar кампаненты. Збор дадзеных каманды, пераклад, структураванне '],
           ['5h', 'Пераклад, даданне дадзеных архітэктараў. Вёрстка футера, дадзеныя футера, пераклад'],
-          ['7h', 'Страница Team: верстка, респонсив, блок параллакс фото команды, заголовок, переводы, изменить вклад для каждого участника'],
+          ['7h', 'Страніца Team: верстка, респонсив, блок параллакс фото команды, заголовок, переводы, изменить вклад для каждого участника'],
           ['8h', 'Стварэнне двух кампанентаў паралакс, даданне на старонку'],
+          ['3.5h', 'Дадзеныя Worklog да абъекта, пераклад дадзеных. Стварэнне табліцы Worklog.'],
+          ['3.5h', 'Самаацэнка з чекбоксамі і спісам ўсіх патрабаванняў да старонкі Worklog'],
+          ['3h', 'Абнаўленьні макета старонкі Worklog.'],
+        ],
+      },
+      {
+        name: 'Ирына',
+        data: [
+          ['3h', 'Структура праекта.'],
+          ['2h', 'Распрацоўка спіса твораў'],
+          ['2h', 'Мадальнае відэа'],
+          ['2h', 'Панэль пошуку'],
+          ['4h', 'Пераклад інтэрфейсу і аўтарскіх дадзеных'],
         ],
       }],
   },
@@ -120,6 +160,8 @@ const worklog = {
           ['1h', 'Перевод данных'],
           ['2.5h', 'Исследовать библиотеку parallax, подготовить изображения блока parallax'],
           ['5.5h', 'Создать макет страницы автора (figma), реализовать блок параллакс главной страницы'],
+          ['4h', 'Работа с блоком шкалы времени, таблица с авторскими работами, видео, блок карты'],
+          ['5h', 'Руководство по стилю: логотип страницы, цвета, шрифты'],
         ],
       },
       {
@@ -129,6 +171,9 @@ const worklog = {
           ['4h', 'Перенос внедренных компоненты на среду gatsby'],
           ['1.5h', 'Исследование storybook, использование для представления типографии и т. д. Фикс storybook'],
           ['1.5h', 'Storybook info-addon, storybook, документация.'],
+          ['3h', 'Реализация компонентов Logo и Header.'],
+          ['2h', 'Реализация компонент поиска.'],
+          ['2h', 'Компонент поиска, рефакторинг.'],
         ],
       },
       {
@@ -140,7 +185,9 @@ const worklog = {
           ['2h', 'Button component'],
           ['2h', 'Перевод данных'],
           ['3h', 'Блок главной страницы, Блок архитектора, Стиль и положение кнопки'],
-
+          ['3.5h', 'Страница Worklog, адаптивная таблица.'],
+          ['4h', 'Блок с трудностями в компоненте Worklog, перевод блока'],
+          ['2h', 'Обновление данных Worklog, перевод обновлений'],
         ],
       },
       {
@@ -164,6 +211,19 @@ const worklog = {
           ['5h', 'Перевод, добавление данных архитекторов. Верстка футера, данные футера, перевод'],
           ['7h', 'Страница Team: верстка, респонсив, блок параллакс фото команды, заголовок, переводы, изменить вклад для каждого участника'],
           ['8h', 'Создание двух parallx компонентов'],
+          ['3.5h', 'Данные Worklog к объекту, перевод данных. Создание таблицы Worklog.'],
+          ['3.5h', 'Самооценка с чекбоксами и списком всех требований к странице Worklog.'],
+          ['3h', 'Обновление макета страницы Worklog.'],
+        ],
+      },
+      {
+        name: 'Ирина',
+        data: [
+          ['3h', 'Структура проекта.'],
+          ['2h', 'Работа со списоком работ'],
+          ['2h', 'Модальное видео'],
+          ['2h', 'Панель поиска'],
+          ['4h', 'Перевод данных интерфейса и автора'],
         ],
       }],
   },
