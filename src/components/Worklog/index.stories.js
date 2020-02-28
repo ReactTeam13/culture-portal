8fddeaeefad582e3dfbe1cb360e856b422f949c5
Artsiom_Reutovich <artsiom_reutovich@epam.com>
2020-02-28 14:25:34 +0300

    refactor: fix lint errors

--- src/components/Worklog/index.stories.js
+++ src/components/Worklog/index.stories.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 
-import WorkLog from './index';
 import WorklogTab from './WorklogTab/index';
 import WorkDifficulties from './WorkDifficulties/index';
 import Requirements from './Requirements/index';
@@ -9,18 +8,14 @@ import Card from './Card/index';
 
 import dataWorklog from '../../data/worklog';
 import dataDifficulties from '../../data/difficulties';
-import dataRequirements from '../../data/difficulties';
 
 
 storiesOf('WorkLog', module)
-  // .add('WorkLog UI representation', () => (
-  //   <WorkLog/>
-  // ))
   .add('WorkLogTabs UI representation', () => (
     <WorklogTab data={dataWorklog.ru.data} />
   ))
   .add('Requirements UI representation', () => (
-    <Requirements data={dataRequirements.ru.data} />
+    <Requirements data={dataDifficulties.ru.data} />
   ))
   .add('Card UI representation', () => {
     const { icon, title, description } = dataDifficulties.ru.data[1];
