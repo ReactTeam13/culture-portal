fb50bcd1887a3245fe1e0ac4d74a64f9113038cf
Ksenia Kuntsevich <kse.kuntsevich@icloud.com>
2020-02-29 19:13:50 +0300

    feat: add storybook link

--- src/containers/StylePage/index.js
+++ src/containers/StylePage/index.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import './index.css';
 import StyleInfo from '../../components/StyleInfo';
+import Title from '../../components/Worklog/Title/index';
 
 function StylePage() {
-  return (<StyleInfo />);
+  const link = (
+    <a
+      className="h font-weight-bold"
+      href="https://eager-spence-9bfadc.netlify.com/?path=/story/*"
+    >
+      Go to StoryBook &gt;&gt;
+    </a>
+  );
+  return (
+    <div>
+      <StyleInfo />
+      <div className="p-5">
+        <Title text={link} />
+      </div>
+    </div>
+  );
 }
 
 export default StylePage;
